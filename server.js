61a4430e4eaf1faab2c5546727745db93d415dc4
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-09-14 20:38:14 -0300

    Add files

--- server.js
+++ server.js
@@ -2,24 +2,36 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+/* ***********************
+ * View engine
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
+app.get("/", function(req, res) {
+res. render ("index", {title: "Home"})})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
+const port = process.env.PORT || 5501;
 const host = process.env.HOST
 
 /* ***********************
@@ -28,3 +40,4 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
