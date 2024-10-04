10d731a6e9d59c1e18bd21dabec634265fdfb532
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-10-03 22:11:21 -0300

    Sessions & Messages

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
 const utilities = require("./utilities/")
 const pool = require('./database/')
-
+const session = require("express-session")
 
 /* ***********************
  * View Engine and Templates
@@ -22,6 +22,27 @@ const pool = require('./database/')
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * Routes
